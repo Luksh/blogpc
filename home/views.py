302c205985a544d197a0ae2ac151b6a5237e0ba9
Luksh <hotmale_luksh@hotmail.com>
2022-04-08 17:25:40 +0545

    second commit

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+app_name = "home"
 
 # Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
